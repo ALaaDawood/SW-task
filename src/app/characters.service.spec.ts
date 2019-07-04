f79a8bacaa54c788d94d4b071aca54f9fce2aa5b
ALaa Dawood <alaa.dawood99@yahoo.com>
2019-07-04 23:58:17 +0200

    Add test Units

--- src/app/characters.service.spec.ts
+++ src/app/characters.service.spec.ts
@@ -1,12 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CharactersService } from './characters.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('CharactersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let charactersService: CharactersService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [CharactersService]
+    });
+    charactersService = TestBed.get(CharactersService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: CharactersService = TestBed.get(CharactersService);
-    expect(service).toBeTruthy();
+    expect(charactersService).toBeTruthy();
   });
+
+  it('should has getfilteredPeople function', () => {
+    expect(charactersService.getFilteredPeople).toBeTruthy();
+   });
+
+
+  it('getFilteredPeople() should GET people', () => {
+
+    const people = {};
+
+    charactersService.getFilteredPeople("r").subscribe((res) => {
+      expect(res).toEqual(people);
+    });
+    const req = httpMock.expectOne(`https://swapi.co/api/people/?search=r`);
+    expect(req.request.method).toEqual("GET");
+    req.flush(people);
+    httpMock.verify();
+  });
+
 });
+
