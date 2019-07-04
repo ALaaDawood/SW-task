0f255297635ce328fe06116a018c889961a624a6
ALaa Dawood <alaa.dawood99@yahoo.com>
2019-07-04 23:59:10 +0200

    Add test units

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,13 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HomeComponent } from './home.component';
+import { DebugElement } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let de : DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule, HttpClientTestingModule ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
@@ -16,10 +21,42 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('has name', ()=> {
+    for (let character of component.filteredChars) {
+        expect(character.name).toBeTruthy();
+    }
+  });  
+
+  it('has url', ()=> {
+    for (let character of component.filteredChars) {
+        expect(character.url).toBeTruthy();
+    }
+  });
+
+  it('has height', ()=> {
+    for (let character of component.filteredChars) {
+        expect(character.height).toBeTruthy();
+    }  
+  });
+
+  it('has homeworld', ()=> {
+    for (let character of component.filteredChars) {
+        expect(character.homeworld).toBeTruthy();
+    }  
+  });
+
+  it('contains the search text', ()=>{
+    for (let character of component.filteredChars) {
+      expect(character.name).toBe(component.searchText);
+    }  
+  });
+
 });
